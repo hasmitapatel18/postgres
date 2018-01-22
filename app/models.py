29c74b766871467ba21f1cdf3a9a51af903a77b9
Hasmita Patel <hasmitapatel@Hasmitas-MacBook-Pro.local>
2018-01-22 23:42:02 +0000

    changed models to set foreign key relationships

--- app/models.py
+++ app/models.py
@@ -5,21 +5,26 @@ class Rep_class(db.Model):
     __tablename__ = 'classes'
     id = db.Column(db.Integer, primary_key=True )
     name = db.Column(db.String(100), nullable=False)
+    rep_families=db.relationship('Rep_family', backref='classes', lazy=True)
 
 class Rep_family(db.Model):
     __tablename__ = 'families'
     id = db.Column (db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    rep_class= db.ForeignKey=('classes.id')
+    rep_class_id= db.Column(db.Integer, db.ForeignKey('classes.id'))
+    rep_class=db.relationship('Rep_class')
+    reps=db.relationship('Rep', backref='families', lazy=True)
 
 class Rep(db.Model):
     __tablename__ = 'reps'
     id = db.Column (db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    rep_family= db.ForeignKey=('families.id')
+    rep_family_id= db.Column(db.Integer, db.ForeignKey('families.id'))
+    rep_family=db.relationship('Rep_family')
+    translated_products=db.relationship('Translated_products', backref='reps', lazy=True)
 
 class Translated_products(db.Model):
     __tablename__ = 'translated_product'
     id = db.Column (db.Integer, primary_key=True)
     name = db.Column(db.String(1000))
-    rep= db.ForeignKey=('reps.id')
+    rep_id= db.Column(db.Integer, db.ForeignKey('reps.id'))
