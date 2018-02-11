b8d2f503e2e3ea021317d52423f058ccf2cb25f7
Hasmita Patel <hasmitapatel@Hasmitas-MacBook-Pro.local>
2018-02-11 13:40:06 +0000

    added comments needs more comments

--- config.py
+++ config.py
@@ -9,15 +9,17 @@ class DevelopmentConfig(Config):
     """
     Development configurations
     """
-#set debug to false in production
+#set debug to true in development. Allows for exceptions to be raised should there be errors and allows for the reloading of the app once it has been updated. set to true in development (the default is set to false)
     DEBUG = True
+
+#allows for the logging of errors when set to true
     SQLALCHEMY_ECHO = True
 
 class ProductionConfig(Config):
     """
     Production configurations
     """
-
+#set debug to false in production
     DEBUG = False
 
 app_config = {
